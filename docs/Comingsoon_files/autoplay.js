d1275baabe996fc2877aeea01540157838667e52
Leo728 <yanze.shi@outlook.com>
2023-05-04 12:17:46 +1000

    1

---
+++ docs/Comingsoon_files/autoplay.js
@@ -0,0 +1,10 @@
+var promise = document.querySelector('audio').play();
+
+if (promise !== undefined) {
+  promise.then(_ => {
+    // Autoplay started!
+  }).catch(error => {
+    // Autoplay was prevented.
+    // Show a "Play" button so that user can start playback.
+  });
+}
